936f87e4933b6077788deb6637a385019b44d6fc
semoleg1986 <semoleg1986@gmail.com>
2025-08-11 22:51:06 +0400

    fix(client): basename url

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -5,19 +5,24 @@ import NotFoundPage from '@pages/NotFound';
 import Layout from '@shared/components/Layout';
 import { ROOT_PAGE, NOT_FOUND_PAGE } from '@shared/constants/routes';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: ROOT_PAGE,
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: NOT_FOUND_PAGE,
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: ROOT_PAGE,
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: NOT_FOUND_PAGE,
-        element: <NotFoundPage />,
-      },
-    ],
-  },
-]);
+    basename: '/client-al2a',
+  }
+);
